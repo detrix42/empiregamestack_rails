543e34853c9cf5ae18c7168509696c2bcc631f4a
detrix42 <detrix42@gmail.com>
2022-03-18 23:12:44 -0400

    ugh

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  Rails.application.config.hosts << "empiregamestack.com"
+  Rails.application.config.hosts << ".empiregamestack.com"
+  puts Rails.application.config.hosts.inspect
 end
