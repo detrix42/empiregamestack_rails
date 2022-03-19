1abaf2ca77bcdcb61a2c8a6411c1dd758ac49543
detrix42 <detrix42@gmail.com>
2022-03-18 22:21:52 -0400

    base landing page quick and dirty start

--- config/puma.rb
+++ config/puma.rb
@@ -8,14 +8,21 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+renv = ENV.fetch("RAILS_ENV_EGS", "development")
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+case renv
+when "development"
+  worker_timeout 3600
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port ENV.fetch("PORT") { 4000 }
+when 'production'
+  puts 'Binding to Empiregamestack socket'
+  bind 'unix:/home/detrix42/www/empiregamestack.sock'
+end
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
