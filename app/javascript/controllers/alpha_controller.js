d06e87b6093b82b6b3897215fb68ff09860e15d1
detrix42 <detrix42@gmail.com>
2022-03-20 13:48:50 -0400

    now splash screen is visible for one second then fades away.

--- app/javascript/controllers/alpha_controller.js
+++ app/javascript/controllers/alpha_controller.js
@@ -4,5 +4,15 @@ import BaseService from "../services/base_service"
 export default class extends Controller {
   connect() {
     window['baseService'] = new BaseService();
+
+    setTimeout( () => {
+      const splash$ = $('.splash')
+      splash$.animate(
+          {opacity: 0},
+          400,
+          () => splash$.remove()
+      )
+    }, 1000)
+
   }
 }
